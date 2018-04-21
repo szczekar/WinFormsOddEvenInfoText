20d63c121eb904af6882914610c4c8f1f7780b2c
Karol Szczepaniak <szczepaniakkarol@gmail.com>
2018-04-21 16:22:03 +0200

    Add files via upload

---
+++ WinFormsOddEvenInfoTxt/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WinFormsOddEvenInfoTxt
+{
+    partial class OddEvenInfoText
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputNumber = new System.Windows.Forms.TextBox();
+            this.buttonTxtInfo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(327, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Put the number below";
+            // 
+            // inputNumber
+            // 
+            this.inputNumber.Location = new System.Drawing.Point(326, 154);
+            this.inputNumber.Name = "inputNumber";
+            this.inputNumber.Size = new System.Drawing.Size(111, 20);
+            this.inputNumber.TabIndex = 1;
+            // 
+            // buttonTxtInfo
+            // 
+            this.buttonTxtInfo.Location = new System.Drawing.Point(300, 220);
+            this.buttonTxtInfo.Name = "buttonTxtInfo";
+            this.buttonTxtInfo.Size = new System.Drawing.Size(168, 23);
+            this.buttonTxtInfo.TabIndex = 2;
+            this.buttonTxtInfo.Text = "Save answer in file";
+            this.buttonTxtInfo.UseVisualStyleBackColor = true;
+            this.buttonTxtInfo.Click += new System.EventHandler(this.buttonTxtInfo_Click);
+            // 
+            // OddEvenInfoText
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.Grip;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonTxtInfo);
+            this.Controls.Add(this.inputNumber);
+            this.Controls.Add(this.label1);
+            this.Name = "OddEvenInfoText";
+            this.Text = "OddEvenInfoText";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputNumber;
+        private System.Windows.Forms.Button buttonTxtInfo;
+    }
+}
+
---
+++ WinFormsOddEvenInfoTxt/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WinFormsOddEvenInfoTxt
+{
+    public partial class OddEvenInfoText : Form
+    {
+        public OddEvenInfoText()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonTxtInfo_Click(object sender, EventArgs e)
+        {
+             int inputUserNumber = Convert.ToInt32(inputNumber.Text);
+             StreamWriter file;
+              
+
+            try
+            {
+
+                file = new StreamWriter(@"info.txt");
+
+                if (inputUserNumber % 2 == 0)
+                {
+
+                    file.WriteLine("Number " + inputUserNumber + " is even !");
+
+                }
+                else
+                {
+                    file.WriteLine("Number " + inputUserNumber + " is odd !");
+
+                }
+
+                MessageBox.Show("File is saved.");
+                file.Close();
+
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("File was not created !");
+            }
+              
+        }
+    }
+}
